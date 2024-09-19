8bb7f4df93ba1b8907a60c0d90973c7f4eaa7c6d
wachinaba <wachinaba@gmail.com>
2024-09-19 21:57:37 +0900

    fix package settings

--- auto_thrust_recorder/setup.py
+++ auto_thrust_recorder/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'prop_coef_finder = auto_thrust_recorder.prop_coef_finder:main',
         ],
     },
 )
