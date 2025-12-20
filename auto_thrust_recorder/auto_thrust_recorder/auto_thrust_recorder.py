7af53d403f21bc00c55e022278627d39b63874d8
wachinaba <wachinaba@gmail.com>
2025-12-20 16:54:31 +0900

    feat: expand thrust coefficient configurations in auto_thrust_recorder.py by adding new slant adjustments for various tilt and fold angles, enhancing the model's flexibility and accuracy in thrust recording.

--- auto_thrust_recorder/auto_thrust_recorder/auto_thrust_recorder.py
+++ auto_thrust_recorder/auto_thrust_recorder/auto_thrust_recorder.py
@@ -42,6 +42,18 @@ class AutoThrustRecorder(Node):
             "30deg_short": [91.475, 21.633, 0.4504],
             "0deg_short": [126.67, 14.089, 0.6691],
             "linear": [0.0, 1.0, 0.0],
+            # slant -15deg
+            ## fold 0deg
+            "tilt0deg_fold0deg_slant-15deg": [111.5, 21.129, 0.7433], #y = 111.5x2 + 21.129x + 0.7433
+            "tilt-15deg_fold0deg_slant-15deg": [108.97, 18.88, 0.8118], #y = 108.97x2 + 18.88x + 0.8118
+            "tilt-30deg_fold0deg_slant-15deg": [100.06, 16.024, 0.6129], #y = 100.06x2 + 16.024x + 0.6129
+
+            ## fold 15deg
+            "tilt15deg_fold15deg_slant-15deg": [108.52, 18.579, 0.7022], #y = 108.52x2 + 18.579x + 0.7022
+            "tilt0deg_fold15deg_slant-15deg": [110.95, 20.003, 0.6545], #y = 110.95x2 + 20.003x + 0.6545
+            "tilt-15deg_fold15deg_slant-15deg": [106.97, 19.331, 0.6339], # y = 106.97x2 + 19.331x + 0.6339
+            "tilt-30deg_fold15deg_slant-15deg": [99.369, 15.017, 0.7342], # y = 99.369x2 + 15.017x + 0.7342
+
             # slant 0deg
             ## fold 0deg
             "tilt-30deg_fold0deg_slant0deg": [103.41, 16.592, 0.5887], #y = 103.41x2 + 16.592x + 0.5887
@@ -68,10 +80,15 @@ class AutoThrustRecorder(Node):
             "tilt23deg_fold15deg_slant0deg": [104.91, 17.476, 0.5441], #104.91x2 + 17.476x + 0.5441
             "tilt30deg_fold15deg_slant0deg": [92.596, 17.961, 0.5213], #92.596x2 + 17.961x + 0.5213
             # slant 15deg
+            ## fold 0deg
+            "tilt-15deg_fold0deg_slant15deg": [110.34, 19.409, 0.6955], #y = 110.34x2 + 19.409x + 0.6955
             "tilt0deg_fold0deg_slant15deg": [110.87, 20.433, 0.8024], #y = 110.87x2 + 20.433x + 0.8024
             "tilt15deg_fold0deg_slant15deg": [108.59, 20.639, 0.669], #y = 108.59x2 + 20.639x + 0.669
             "tilt30deg_fold0deg_slant15deg": [99.874, 17.767, 0.6262], #y = 99.874x2 + 17.767x + 0.6262
-
+            ## fold 15deg
+            "tilt0deg_fold15deg_slant15deg": [115.3, 19.694, 0.5929], #y = 115.3x2 + 19.694x + 0.5929
+            "tilt15deg_fold15deg_slant15deg": [108.23, 20.094, 0.7166], #y = 108.23x2 + 20.094x + 0.7166
+            "tilt30deg_fold15deg_slant15deg": [101.4, 17.914, 0.4998], #y = 101.4x2 + 17.914x + 0.4998
         }
 
         self.scheduler_params = {
